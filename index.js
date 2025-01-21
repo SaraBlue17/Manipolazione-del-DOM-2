037d2e2b79f674c6f47736e3feaa7981703f0ad0
Sara Rampazzo <sararampazzo.ph@gmail.com>
2025-01-21 01:06:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Implementare il codice necessario per:
+
+// Recuperare il valore di ciascun campo di input e creare un oggetto person contenente le proprietà: firstName, lastName e age.
+
+const inputs = document.getElementsByClassName("form-input");
+const firstName = inputs[0];
+const lastName = inputs[1];
+const age = inputs[2];
+
+const person = {
+  firstName: firstName.value,
+  lastName: lastName.value,
+  age: age.value,
+};
+
+// Infine recuperare l'elemento form aggiungere l'attributo data-person contenente l'oggetto person in formato json.
+
+const form = document.querySelector("form");
+form.setAttribute("data-person", JSON.stringify(person));
+
+console.log(person);
